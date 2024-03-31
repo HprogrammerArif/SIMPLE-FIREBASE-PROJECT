bb459bd1c4c12a54f1aac60d609c198a6e7a9b2c
HprogrammerArif <programmerhabluarif@gmail.com>
2024-03-31 19:43:13 +0600

    compleate google authientication by firebase

--- src/--steps.js
+++ src/--steps.js
@@ -1,13 +1,31 @@
 /**
+ *       ----------
+ *      INITIAL SETUP
+ *     ----------------
+ * 
  * 1. visit: console.firebase.google.com
  * 2: create a project (skip google analytics)
  * 3: register app (create config)
  * 4. install firebase: npm install firebase
  * 5. add config file to your project
  * 6. DANGER: do not publish or make firebase config to punlic by pushing those to github
+ * 
+ *     --------
+ *    INTREGRATION
+ *   ----------------
+ * 
  * 7. Visit: go to doc> build> authentication > web > get started .
  * 8. export app from the firebase.config.js file: export default app.
  * 9: Login.jsx: import getAuth from firebase/auth
  * 10. Create: const auth = getAuth(app)
+ * 
+ *    -------------------
+ *    PROVIDER SETUP
+ *    ---------------------
+ * 
  * 11. Import google auth provider and create a new provider
+ * 12. Use signInWithPopUp and pass auth and provider
+ * 13. Activate signin method (google, facebook, github etc)
+ * 14. [vite]: change 127.0.0.1 to localhost
+ * 
  */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
-import app from '../../firebase/firebase.init';
+import React, { useState } from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import app from "../../firebase/firebase.init";
 
 const Login = () => {
-   const auth = getAuth(app);
-   const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
 
-   const handleGoogleSignIn = () => {
-    console.log('Google mama is coming');
-   }
+  const auth = getAuth(app);
+  console.log(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
+      })
+
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(result => {
+      console.log(result);
+      setUser(null)
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
 
   return (
     <div>
-      <button onClick={handleGoogleSignIn}>Google Login</button>
+      {/* user? sign out: sign in */}
+      {
+        user? <button onClick={handleSignOut}>Sign out</button>:<button onClick={handleGoogleSignIn}>Google Login</button>
+      }
+      
+      
+      {user && (
+        <div>
+          <h3>User: {user.displayName}</h3>
+          <h3>Email: {user.email}</h3>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
